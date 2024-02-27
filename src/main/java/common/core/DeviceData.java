4e77762490ddc888c32b9c38521f3e1474a42994
sidd_jain <sidd_jain@hotmail.com>
2024-02-27 16:27:07 -0500

    Setup updates

---
+++ src/main/java/common/core/DeviceData.java
@@ -0,0 +1,7 @@
+package common.core;
+
+public class DeviceData {
+    private String DEFAULT_DEVICE = "Google Pixel 7";
+    private final String DEFAULT_ORIENTATION = "Portrait";
+    private final String DEFAULT_OS_VERSION="13.0";
+}
---
+++ src/main/java/common/core/ExecutionListener.java
@@ -0,0 +1,7 @@
+package common.core;
+
+import com.aventstack.extentreports.ExtentReports;
+import org.testng.IExecutionListener;
+public class ExecutionListener implements IExecutionListener{
+    public static ExtentReports report;
+}
